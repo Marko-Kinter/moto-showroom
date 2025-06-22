a098174c9535e891cbe97d3547735d8ff4a8e504
Marko-Kinter <kintermarko@gmail.com>
2025-06-21 22:10:18 -0300

    fix slug page

--- app/moto/[slug]/page.tsx
+++ app/moto/[slug]/page.tsx
@@ -1,16 +1,18 @@
-import FeatureTable from '@/components/products/components/FeaturesTable';
+import FeatureTable from "@/components/products/components/FeaturesTable";
 
-type PageProps = {
-  params: { slug: string };
+type Props = {
+  params: {
+    slug: string;
+  };
 };
 
-export default async function MotoPage({ params }: PageProps) {
-  const { slug } = params;
+export default async function MotoPage({params}: { params : Promise<{slug:string}>}) {
+  const { slug } = await params;
 
   return (
     <main>
       <p>Info de moto: {slug}</p>
-      <FeatureTable slug={slug} />
+      <FeatureTable slug={slug}/>
     </main>
   );
 }
