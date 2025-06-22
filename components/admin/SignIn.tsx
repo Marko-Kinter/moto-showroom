fe0e903160bc597a7e8aae0e28f3a66432a0d4b4
Marko-Kinter <kintermarko@gmail.com>
2025-06-21 22:14:13 -0300

    fix login page

--- components/admin/SignIn.tsx
+++ components/admin/SignIn.tsx
@@ -3,5 +3,5 @@
 import { signIn } from "next-auth/react"
  
 export default function SignIn() {
-  return <button onClick={() => signIn("google")}></button>
+  return <button onClick={() => signIn("google")}>Sign In with google</button>
 }
